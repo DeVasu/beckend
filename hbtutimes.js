3a12170ee0850e006e4b7a9f5ed7f8642002107e
Ashish singh <ashishofficial47@gmail.com>
2018-08-15 13:57:36 +0530

    files ADDEDD
    All files images icons are add in this repo

---
+++ hbtutimes.js
@@ -0,0 +1,14 @@
+
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 50){
+        ('wrapper').addClass('header');
+    }
+    else{
+       ('wrapper').removeClass('header');
+    }
+})
+
+onclick.getElementByClass('rankethon'){
+    visit:'www.rankethon.com'
+}
