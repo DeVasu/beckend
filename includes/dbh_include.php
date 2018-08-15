0d0e212e3679c51b5322f7a0d9fdca8443cbe8c4
Ashish singh <40070226+Logan-47@users.noreply.github.com>
2018-08-15 13:32:38 +0530

    dbh_include.php
    mysqli connection  default for all pages

---
+++ includes/dbh_include.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbpassword = "";
+$dbname = "hbtutimes";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbpassword, $dbname);
+
+
+?>
