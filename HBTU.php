d5e2cd3325c02e22329d23f4ac08ed0ebf003051
Ashish singh <40070226+Logan-47@users.noreply.github.com>
2018-08-15 13:34:25 +0530

    update hbtu.php
    date input format updated

--- HBTU.php
+++ HBTU.php
@@ -47,17 +47,50 @@
                             <input type="text" size="40" name="email" placeholder="EMail I'd" />
                             <input type="radio" name="gender" value="male" placeholder="Male" /><span id="m-f"> Male </span>
                             <input type="radio" name="gender" value="female" /> <span id="m-f">Female </span>                                  <br />
-                            <!-- <blockquote><span style="color:grey">DOB</span> <input type="date" placeholder="date" name="dob"></blockquote> -->
-                             <span id="dob"> Date Of Birth: </span> <br/>
+                            <blockquote><span style="color:grey">DOB</span> 
+                            <input type="date" placeholder="date" name="date">
+                            </blockquote>
+                             <!-- <span id="dob"> Date Of Birth: </span> <br/>
                             <select name="day">
-                                <option selected="selected">Day</option> <option value=1 >01</option> <option value=2 >02</option> <option value=3 >03</option> <option value=4 >04</option> <option value=5 >05</option> <option value=6 >06</option> <option value=7 >07</option> <option value=8 >08</option> <option value=9 >09</option> <option value=10 >10</option> <option value=11 >11</option> <option value=12 >12</option> <option value=13 >13</option> <option value=14 >14</option> <option value=15 >15</option> <option value=16 >16</option> <option value=17 >17</option> <option value=18 >18</option> <option value=19 >19</option> <option value=20 >20</option> <option value=21 >21</option> <option value=22 >22</option> <option value=23 >23</option> <option value=24 >24</option> <option value=25 >25</option> <option value=26 >26</option> <option value=27 >27</option> <option value=28 >28</option> <option value=29 >29</option> <option value=30 >30</option> <option value=31 >31</option>
+                                <option selected="selected">Day</option> 
+                                <option value="01">01</option> 
+                                <option value="02">02</option> 
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                                <option value="07">07</option> 
+                                <option value="08">08</option> 
+                                <option value="09">09</option> 
+                                <option value="10">10</option> 
+                                <option value="11">11</option> 
+                                <option value="12">12</option> 
+                                <option value="13">13</option> 
+                                <option value="14">14</option> 
+                                <option value="15">15</option> 
+                                <option value="16">16</option> 
+                                <option value="17">17</option> 
+                                <option value="18">18</option> 
+                                <option value="19">19</option> 
+                                <option value="20">20</option> 
+                                <option value="21">21</option> 
+                                <option value="22">22</option> 
+                                <option value="23">23</option> 
+                                <option value="24">24</option> 
+                                <option value="25">25</option> 
+                                <option value="26">26</option> 
+                                <option value="27">27</option> 
+                                <option value="28">28</option> 
+                                <option value="29">29</option> 
+                                <option value="30">30</option> 
+                                <option value="31">31</option>
                             </select> 
                             <select name="month">
                                 <option selected="selected">Month</option> <option value="january">January</option> <option value="february">February</option> <option value="March">March</option> <option value="april">April</option> <option value="may">May</option> <option value="june">June</option> <option value="july">July</option> <option value="august">August</option> <option value="September">September</option> <option value="october">October</option> <option value="november">November</option> <option value="december">December</option>
                             </select>
                             <select name="year">
                                     <option selected="selected">Year</option> <option value="2017">2017</option> <option value="2016">2016</option> <option value="2015">2015</option> <option value="2014">2014</option> <option value="2013">2013</option> <option value="2012">2012</option> <option value="2011">2011</option> <option value="2010">2010</option> <option value="2009">2009</option> <option value="2008">2008</option> <option value="2007">2007</option> <option value="2006">2006</option> <option value="2005">2005</option> <option value="2004">2004</option> <option value="2003">2003</option> <option value="2002">2002</option> <option value="2001">2001</option> <option value="2000">2000</option> <option value="1999">1999</option> <option value="1998">1998</option> <option value="1997">1997</option> <option value="1996">1996</option> <option value="1995">1995</option> <option value="1994">1994</option> <option value="1993">1993</option> <option value="1992">1992</option> <option value="1991">1991</option> <option value="1990">1990</option> <option value="1989">1989</option> <option value="1988">1988</option> <option value="1987">1987</option> <option value="1986">1986</option> <option value="1985">1985</option> <option value="1984">1984</option> <option value="1983">1983</option> <option value="1982">1982</option> <option value="1981">1981</option> <option value="1980">1980</option> 
-                            </select> <br />
+                            </select> <br /> -->
                             <input type="password" name="password" size="20" placeholder="Create Password" required="required" />
                             <input type="password" name="password2" size="20" placeholder="Confirm Password" required="required" />
                         <input type="submit" name="Sign Up" placeholder="Sign Up " value="Sign Up for HT" />
